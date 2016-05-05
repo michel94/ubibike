059ced829eda712a49f1c14b263e6bfe6342259e
Miguel Martins Duarte <miguel.duarte20@hotmail.com>
2016-05-05 22:47:27 +0100

    Added some more rest api calls to the server

--- server/routes/db.js
+++ server/routes/db.js
@@ -10,23 +10,70 @@ var Id = Schema.Types.ObjectId;
 var userSchema = new Schema({
     username: String,
     password: String,
-    name: String,
+    name: {type: String, default: 'undefined'},
     score: Number,
     distance: Number,
     trajectories: []
 });
 
 var bikeSchema = new Schema({
-	station: Id
+	station: Id,
+	name: String,
+	reservedBy: Id
 });
 
 var stationSchema = new Schema({
-	bikes: [Id],
+	location: {latitude: Number,
+			   longitude: Number},
+	name: String
+});
 
-})
+var processedTransactions = new Schema({
+	srcUser: Id,
+	srcMessageId: Number
+});
 
 User = mongoose.model('User', userSchema);
 Bike = mongoose.model('Bike', bikeSchema);
 Station = mongoose.model('Station', stationSchema);
+
+function populateDB(){
+	var stations = [{name: "Station 1", location: [38.7738718, -9.0958291]},
+					{name: "Station 2", location: [38.7354228, -9.1466685]},
+					{name: "Station 3", location: [38.7334772, -9.1366397]},
+					{name: "Station 4", location: [38.7552882, -9.1163247]}];
+
+	var bikes =	[{name: "bike 1", reservedBy: null},
+				{name: "bike 2", reservedBy: null},
+				{name: "bike 3", reservedBy: null},
+				{name: "bike 4", reservedBy: null}];
+
+	for(var i=0; i<stations.length; i++){
+		var s = stations[i];
+		var station = new Station({
+			name: s.name,
+			location: {latitude: s.location[0], longitude: s.location[1]} 
+		});
+		console.log(i);
+		station.save(function(e, nstation){
+			var b = bikes[Number(nstation.name.split(' ')[1])-1];
+			console.log(i, bikes);
+			var bike = new Bike({
+				name: b.name,
+				reservedBy: null,
+				station: nstation._id
+			});
+			bike.save();
+			
+		});
+	}
+}
+
+Station.count(function (err, count) {
+    if (count == 0) {
+    	console.log("Empty collection");
+        populateDB();
+    }
+});
 
 module.exports = {db: db, User: User, Bike: Bike, Station: Station};
--- server/routes/index.js
+++ server/routes/index.js
@@ -70,5 +70,65 @@ router.post('/login', function(req, res, next){
 		})
 });
 
+router.get('/stations', function(req, res, next){
+	Station.find({}, {}, function(e, docs){
+		res.json({stations: docs});
+	});
+});
+
+router.get('/stationInfo', function(req, res, next){
+	var data = req.body;
+	Station.findById(data.stationId, function(e, station){
+		Bikes.find({station: stationId}, {}, function(e, bikes){
+			res.json({station: station, bikes: bikes});
+		})
+	});
+})
+
+router.get('/requestbike', function(req, res, next){
+	var data = req.body;
+});
+
+/* Transactions, in the format:
+[{srcUser: userId, destUser: userId, timestamp: date, srcMessageId: int, quantity: int}, ...] 
+*/
+router.get('/transactions', function(req, res, next){
+	var data = req.body;
+	for(var i=0; i<data.transactions.length; i++){
+		var srcUser = transactions[i].srcUser;
+		var destUser = transactions[i].destUser;
+		var srcMessageId = transactions[i].srcMessageId;
+		var quant = transactions[i].quantity;
+
+		User.find({_id: {$in: [srcUser, destUser] }}, function(e, docs){
+			if(docs.length == 0){
+				console.log("Cannot complete transaction: src user " + srcUser + " and dest user " + destUser + " don't exist.");
+			}else if(docs.length == 1){
+				if(docs[0]._id == destUser)
+					console.log("Cannot complete transaction: src user " + srcUser + " doesn't exist.");
+				else
+					console.log("Cannot complete transaction: dest user " + destUser + " doesn't exist.");
+			}else{
+				
+				var [src, dest] = (docs[0]._id == srcUser) ? [docs[0], docs[1]] : [docs[1], docs[0]];
+
+				processedTransactions.findOne({srcUser: srcUser, srcMessageId: srcMessageId}, {}, function(e, docs){
+					if(docs.length == 0){ // fresh transaction
+						src.score -= quantity;
+						dest.score += quantity;
+						src.save();
+						dest.save();
+					}else{ // old transaction
+						console.log("Old transaction, ignoring");
+
+					}
+				});
+			}
+
+		});
+		res.json({success: true});
+	}
+});
+
 
 module.exports = router;
