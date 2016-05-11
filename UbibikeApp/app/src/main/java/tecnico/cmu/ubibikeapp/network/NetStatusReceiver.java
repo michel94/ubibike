66dbf6a23736d14259e9c6dc5d867820d68ca419
Miguel Martins Duarte <miguel.duarte20@hotmail.com>
2016-05-11 18:52:24 +0100

    termite script

--- UbibikeApp/app/src/main/java/tecnico/cmu/ubibikeapp/network/NetStatusReceiver.java
+++ UbibikeApp/app/src/main/java/tecnico/cmu/ubibikeapp/network/NetStatusReceiver.java
@@ -5,11 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.util.Log;
 
-/**
- * Created by michel on 5/7/16.
- */
 public class NetStatusReceiver extends BroadcastReceiver {
     private final NetworkListener callback;
 
@@ -19,15 +15,11 @@ public class NetStatusReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.d("Net", "Ayy lmao");
 
         ConnectivityManager connectivityManager = (ConnectivityManager)
                 context.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo activeNetInfo = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
         boolean isConnected = activeNetInfo != null && activeNetInfo.isConnectedOrConnecting();
-        if (isConnected)
-            Log.i("NET", "connected " + isConnected);
-        else Log.i("NET", "Not connected " + isConnected);
 
         if(callback != null)
             callback.onConnection(isConnected);
